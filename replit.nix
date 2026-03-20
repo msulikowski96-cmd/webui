c3ad6db41f5154b2a171603b622aee92cc900bc9
katarzynatycBIG <55509532-katarzynatycBIG@users.noreply.replit.com>
2026-03-20 16:03:20 +0000

    Restored to '3852092b7d8148f69a70348051d08e64d51b2671'
    Replit-Restored-To: 3852092b7d8148f69a70348051d08e64d51b2671

--- replit.nix
+++ replit.nix
@@ -1,6 +1,5 @@
 { pkgs }: {
   deps = [
-    pkgs.nano
     pkgs.python311
     pkgs.git
   ];
--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+# pobierz extensions jeśli nie istnieją
 if [ ! -d "extensions" ]; then
   git clone https://github.com/Fu-Jie/openwebui-extensions.git extensions
 fi
 
-open-webui serve --host 0.0.0.0 --port 5000
+python main.py
